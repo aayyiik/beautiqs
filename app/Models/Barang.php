3e615d0a2af308093430020227ce9d7404c531a0
Ari Lathifatul Chusna <72559349+aayyiik@users.noreply.github.com>
2021-10-12 08:47:02 +0700

    Add files via upload

---
+++ app/Models/Barang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+    protected $table = 'barang';
+    protected $fillable = ['kode_barang','nama_barang','attribute_stok_barang29','harga_beli_barang','harga_jual_barang'];
+}
