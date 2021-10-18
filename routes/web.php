460410df0c0507b7a8f19e0496c6a883d8922de5
Ari Lathifatul Chusna <72559349+aayyiik@users.noreply.github.com>
2021-10-18 20:59:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/barang','BarangController@index');
+//kota
+Route::get('/kota',[KotaController::class,'index'] );
+Route::get('/kota/create',[KotaController::class,'create']) ;
+Route::get('kota/{id}/edit', [KotaController::class,'edit']);
+Route::post('kota/{id}/update', [KotaController::class,'update']);
+Route::get('kota/{id}/delete', [KotaController::class,'delete']);
+
+Route::get('/supplier','SupplierController@index');
+Route::get('/role','RoleController@index');
+Route::get('/login','LoginController@index');
+Route::get('/jenisbarang','JenisBarangController@index');
+Route::get('/role','RoleController@index');
+Route::get('/jenisbarang','JenisBarangController@index');
+Route::get('/penerimaan','penerimaanController@index');
+Route::get('/pembayaran','PembayaranController@index');
+Route::get('/pemesanan','PemesananController@index');
+Route::get('/ukuran','UkuranController@index');
+Route::get('/warna','WarnaController@index');
